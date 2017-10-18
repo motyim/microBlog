e88eb4d8150ff8fd39cf997c5197ac1f94d15062
Mohamed MotYim <mohamed.motyim@gmail.com>
2017-10-18 11:03:05 +0200

    no message

--- src/main/java/me/motyim/learn/microblog/spark/Main.java
+++ src/main/java/me/motyim/learn/microblog/spark/Main.java
@@ -1,8 +1,25 @@
 package me.motyim.learn.microblog.spark;
 
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import static spark.Spark.*;
+
 /**
  * @author MA MotYim <mohamed.motyim@gmail.com>
  * @since 18-Oct-17
  */
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        
+        get("/hello", (req, res) -> "Hello World..");
+
+
+
+
+    }
 }
